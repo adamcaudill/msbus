4c040773717558bf6d3e3190b96bb3351aa5bfd9
Adam Caudill <adam@adamcaudill.com>
2011-11-29 00:54:10 -0500

    Creating a module for the API - the naming is rather asinine at this point, so there's a very good chance that when I've had more sleep, a lot of this will change. But right now I'm most concerned about seeing this doing something - then I'll make it do it right.

--- src/Server/Service.cs
+++ src/Server/Service.cs
@@ -10,7 +10,8 @@ namespace MSBus.Server
   {
     public Service()
     {
-      Get["/"] = _ => { return "Hello World"; };
+      Get["/"] = _ => { return "<html><head><title>Welcome to MSBus!</title></head><body>Welcome to MSBus, the lightweight message queueing and transport system.</body></html>"; };
+      Get["/version"] = _ => { return "MSBus v0.0"; };
     }
   }
 }
---
+++ src/Server/ServiceRest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nancy;
+
+namespace MSBus.Server
+{
+  public class ServiceRest : NancyModule
+  {
+    public ServiceRest() : base("/api1")
+    {
+      
+    }
+  }
+}
