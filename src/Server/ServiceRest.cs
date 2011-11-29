3e4418d953b2338e4afe1a36c57efb459ad43a23
Adam Caudill <adam@adamcaudill.com>
2011-11-29 01:30:21 -0500

    Let's start speaking a little Json

--- src/Server/ServiceRest.cs
+++ src/Server/ServiceRest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Nancy;
+using Nancy.Responses;
 
 namespace MSBus.Server
 {
@@ -10,7 +11,13 @@ namespace MSBus.Server
   {
     public ServiceRest() : base("/api1")
     {
-      
+      Get["/version"] = _ => { return _GetVersion(); };
+    }
+
+    private Response _GetVersion()
+    {
+      var res = new {Version = "0.0"};
+      return new JsonResponse(res, new DefaultJsonSerializer());
     }
   }
 }
